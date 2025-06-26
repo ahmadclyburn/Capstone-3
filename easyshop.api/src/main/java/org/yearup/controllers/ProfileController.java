a9d5c555a45e86ec4f5da36bec85c43e4278e382
Ahmad Clyburn <ahmadclyburn61@gmail.com>
2025-06-26 15:48:57 -0400

    final commit

--- easyshop.api/src/main/java/org/yearup/controllers/ProfileController.java
+++ easyshop.api/src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,10 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.ProfileDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.Profile;
@@ -38,7 +35,16 @@ public class ProfileController {
         {
             throw new RuntimeException(e);
         }    }
-//    private Profile updateProfile(Principal principal){
-//
-//    }
+    @PutMapping()
+    public Profile updateProfile(Principal principal) {
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            Profile profile = profileDao.getByUserId(user.getId());
+            return profileDao.update(profile);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
